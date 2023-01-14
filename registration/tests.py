529ab5d7b939bc824ebf43f660f8831996bcb166
daytnclp98 <dayanna.tnclp@eduge.ch>
2023-01-14 13:41:41 +0100

    Création test du modèle

--- registration/tests.py
+++ registration/tests.py
@@ -1,5 +1,8 @@
+from django.db.models import Model
 from django.test import TestCase
 from django.urls import resolve
+
+from registration.models import Inscription
 from registration.views import tarifs
 #testerUrl
 class URLTest(TestCase):
@@ -14,4 +17,21 @@ class URLTest(TestCase):
 
 
 
+#testerVues
+class ViewTest(TestCase):
 
+    def test_tarifs_a_titre(self):
+        response = self.client.get('/tarifs/')
+        self.assertContains(response, 'Nos tarifs')
+
+    def test_tarifs_utilise_template(self):
+        response = self.client.get('/tarifs/')
+        self.assertTemplateUsed(response, 'tarifs.html')
+
+#TesterModels
+
+
+
+class ModelTest(TestCase):
+    def test_inscription_model_existe(self):
+        self.assertIsInstance(Inscription(), Model)
--- registration/views.py
+++ registration/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def tarifs(request):
-    return HttpResponse()
+    return render(request, 'tarifs.html')
