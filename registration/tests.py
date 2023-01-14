bbe99e6b9db7dd3e79f4869aeeb429d3c94e126d
daytnclp98 <dayanna.tnclp@eduge.ch>
2023-01-14 12:59:20 +0100

    Test des url

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+#testerUrl
+
+class URLTest(TestCase):
+    def tester_les_tarifs_200(self):
+        response = self.client.get('/tarifs/')
+        self.assertEqual(200, response.status_code)
+
+
+    def test_tarifs_appelle_vue(self):
+        response = self.client.get('/tarifs/')
+        self.assertEqual()
+
+
+
+
